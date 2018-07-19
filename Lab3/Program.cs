bb92bfbfae2ab5b0f7951cd1f8925d54a5d67ec0
Lamarcross22 <41022434+Lamarcross22@users.noreply.github.com>
2018-07-19 10:35:35 -0400

    initial commit

---
+++ Lab3/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number 1 and 100.");
+            int number = int.Parse(Console.ReadLine());
+            //put the rnage first 
+
+            if (number % 2 == 1)
+            {
+                Console.WriteLine("Odd");
+            }
+
+
+            else
+            {
+
+
+                if
+                   (number < 25)
+                { Console.WriteLine("Even and less than 25"); }
+                else if (26 number 60 )
+                {
+
+
+                }
+                else
+                {
+                    Console.WriteLine("even");
+                }
+
+            }
+        }
+
+    }
+}
